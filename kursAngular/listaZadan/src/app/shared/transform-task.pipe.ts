b2976750de7a69062c55e6c112f4107e651c4085
puchymiak <puchymiak@gmail.com>
2019-05-25 14:59:42 +0200

    Pipe - przekazujemy argumenty, które dodatkowo zostaną wyświetlone i podłaczamy pipe pod done-task

--- kursAngular/listaZadan/src/app/shared/transform-task.pipe.ts
+++ kursAngular/listaZadan/src/app/shared/transform-task.pipe.ts
@@ -5,9 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TransformTaskPipe implements PipeTransform {
 
-  transform(value: string, args?: any): any {
+  transform(value: string, args?: string = ''): any {
     // bierzemy pierwsza literkę i zamieniamy na duża, nastepnie dołaczamy reszte
-    return value.charAt(0).toUpperCase() + value.slice(1);
+    return value.charAt(0).toUpperCase() + value.slice(1) + args;
   }
 
 }
