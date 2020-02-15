7466b47718d0ce4e0959b538b08e8afce7df9d07
Radek <radeklejman123@gmail.com>
2020-02-15 23:08:06 +0100

    Implement of new popup login in the login component - basic version

--- kursAngular/listaZadan/src/app/app.component.ts
+++ kursAngular/listaZadan/src/app/app.component.ts
@@ -1,24 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { TasksService } from './services/tasks.service';
-import { AuthService } from './auth/auth.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { TasksService } from "./services/tasks.service";
+import { AuthService } from "./auth/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
   // providers: [TasksService] przeniesiony do @ng_module
 })
 export class AppComponent {
+  loginBtn = false;
 
   constructor(public authService: AuthService, private router: Router) {
-    console.log('Wykonuję app.component.ts constructor #1');
+    console.log("Wykonuję app.component.ts constructor #1");
   }
 
   logout() {
-    console.log('Wykonuję app.component.ts logout() #1');
+    console.log("Wykonuję app.component.ts logout() #1");
     this.authService.logout();
-    this.router.navigate(['/login']);
+    this.router.navigate(["/login"]);
   }
-
+  // !
+  activeLogin() {
+    this.loginBtn = !this.loginBtn;
+    console.log(this.loginBtn);
+  }
 }
