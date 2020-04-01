e557d3f161b389d15bad1c02a1195d7e1fac47c0
Radek <radeklejman123@gmail.com>
2020-04-01 17:41:41 +0200

    Poprawa i implementacja sortujacego pipe

--- kursAngular/listaZadan/src/app/shared/sort-name.pipe.ts
+++ kursAngular/listaZadan/src/app/shared/sort-name.pipe.ts
@@ -1,23 +1,24 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { Task } from '../models/task';
+import { Pipe, PipeTransform } from "@angular/core";
+import { Task } from "../models/task";
 
- // unikać pure: false ponieważ bardzo obciaża, nawet przy ruchu myszka sortuje za każdym razem od nowa
+// unikać pure: false ponieważ bardzo obciaża, nawet przy ruchu myszka sortuje za każdym razem od nowa
 
 @Pipe({
-  name: 'sortName',
+  name: "sortName",
   pure: true
 })
 export class SortNamePipe implements PipeTransform {
+  transform(value: Array<Task>, args = false): Array<Task> {
+    if (args) {
+      return value.sort((a, b) => {
+        if (a.name > b.name) {
+          return 1;
+        } else {
+          return -1;
+        }
+      });
+    }
 
-  transform(value: Array<Task>, args?: any): Array<Task> {
-    // a i b to argumenty, czyli 2 różne taski
-    return value.sort((a, b) => {
-      if (a.name.toLowerCase() > b.name.toLowerCase()) {
-        return 1;
-      } else {
-        return -1;
-      }
-    });
+    return value;
   }
-
 }
